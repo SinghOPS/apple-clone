54b386eac2c20b5d376706a5785c46fd8ae15991
Sahajpreet Singh Khasria <125519861+SinghOPS@users.noreply.github.com>
2024-08-01 22:44:16 -0700

    edit: project files

--- src/components/IPhone.jsx
+++ src/components/IPhone.jsx
@@ -11,7 +11,7 @@ import { useGLTF, useTexture } from "@react-three/drei";
 import * as THREE from 'three';
 //public\models\scene.glb 
 function Model(props) {
-  const { nodes, materials } = useGLTF("public/models/scene.glb");
+  const { nodes, materials } = useGLTF("/apple-clone/models/scene.glb");
   const texture = useTexture(props.item.img);
 
   useEffect(() => {
@@ -257,4 +257,4 @@ function Model(props) {
 
 export default Model;
 
-useGLTF.preload("public/models/scene.glb");
+useGLTF.preload("/apple-clone/models/scene.glb");
