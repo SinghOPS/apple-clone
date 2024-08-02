bf45f625ec48d93ce622a415f997a43f217977f8
Sahajpreet Singh Khasria <125519861+SinghOPS@users.noreply.github.com>
2024-08-01 18:30:16 -0700

    add: project files

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/apple-clone/',
   plugins: [react(), sentryVitePlugin({
     org: "sahaj-6t",
     project: "javascript-react"
